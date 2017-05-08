2aa40d26ad5bdca01599221d12e27fb10d8f9ead
David Green <dave@activelylazy.co.uk>
2017-05-08 13:21:38 +0100

    Get react devtools working

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -2,5 +2,7 @@ import rootReducer from '../reducers/root-reducer';
 import {createStore} from 'redux';
 
 export default (initialState) => {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 };
