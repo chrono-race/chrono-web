c508fb9ee600cee55db017c66f00e070c587d305
David Green <dave@activelylazy.co.uk>
2017-05-09 20:22:03 +0100

    Added basic socket-io integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from './store/configure-store';
 import './index.css';
+import io from 'socket.io-client';
 
 const store = configureStore();
 
@@ -13,3 +14,11 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+const socket = io.connect('http://localhost:8000/');
+socket.on('backlog', function(data) {
+  console.log('Got backlog:', data.events);
+});
+socket.on('events', function(data) {
+  console.log('Got events:', data.events);
+})
