8e839c0bc8993b682eeb1e67ec642a3ed33be0a0
David Green <dave@activelylazy.co.uk>
2017-05-10 12:55:52 +0100

    Wire up incoming data to actions

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configure-store';
 import './index.css';
 import io from 'socket.io-client';
+import { backlogReceived, eventsReceived } from './actions/data-actions';
 
 const store = configureStore();
 
@@ -18,7 +19,9 @@ ReactDOM.render(
 const socket = io.connect('http://localhost:8000/');
 socket.on('backlog', function(data) {
   console.log('Got backlog:', data.events);
+  store.dispatch(backlogReceived(data.events));
 });
 socket.on('events', function(data) {
   console.log('Got events:', data.events);
+  store.dispatch(eventsReceived([data.events]));
 })
