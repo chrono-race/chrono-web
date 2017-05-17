e8e2405d956a63b35b3d1d1d85b427caaf5b9f82
David Green <dave@activelylazy.co.uk>
2017-05-17 18:32:58 +0100

    Show sector times to 3dp

--- src/driver-history/DriverHistoryLap.js
+++ src/driver-history/DriverHistoryLap.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 
+function toSectorTime(t) {
+  if (t === null || t === undefined) {
+    return "";
+  }
+  return t.toFixed(3);
+}
+
 class DriverHistoryLap extends Component {
   render() {
     const { lap } = this.props;
     return (
       <tr>
         <td>{lap.get('lapNumber')}</td>
-        <td>{lap.get('s1Time')}</td>
-        <td>{lap.get('s2Time')}</td>
-        <td>{lap.get('s3Time')}</td>
+        <td>{toSectorTime(lap.get('s1Time'))}</td>
+        <td>{toSectorTime(lap.get('s2Time'))}</td>
+        <td>{toSectorTime(lap.get('s3Time'))}</td>
         <td>{lap.get('lapTime')}</td>
       </tr>
     );
