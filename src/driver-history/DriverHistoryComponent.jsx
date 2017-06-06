4fbc9047811613b5ef4a859e256fb190b796b976
David Green <dave@activelylazy.co.uk>
2017-06-06 18:22:38 +0100

    Keep scrollbar at bottom of driver history on update

--- src/driver-history/DriverHistoryComponent.jsx
+++ src/driver-history/DriverHistoryComponent.jsx
@@ -1,43 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import Immutable from 'immutable';
 import DriverHistoryLap from './DriverHistoryLap';
 
-const DriverHistory = ({ driver, sessionBests }) => {
-  const dhLaps = [];
-  if (driver === undefined) {
-    return (<div>No driver selected</div>);
+class DriverHistory extends Component {
+  componentWillUpdate() {
+    const node = this.scrollingContainer;
+    this.shouldScrollToBottom = node != null &&
+      (node.scrollTop + node.offsetHeight === node.scrollHeight);
   }
-  const driverBests = driver.get('best');
-  driver.get('laps').toArray().forEach((lap) => {
-    dhLaps.push(<DriverHistoryLap key={lap.get('lapNumber')} lap={lap} driverBests={driverBests} sessionBests={sessionBests} />);
-  });
+  componentDidUpdate() {
+    if (this.shouldScrollToBottom) {
+      const node = this.scrollingContainer;
+      node.scrollTop = node.scrollHeight;
+    }
+  }
 
-  return (
-    <div className="flex-container">
-      <div className="timing-table-header">
-        <table className="table table-striped timing-table driver-history">
-          <thead>
-            <tr>
-              <th className="lapNumber">#</th>
-              <th className="s1Time">s1</th>
-              <th className="s2Time">s2</th>
-              <th className="s3Time">s3</th>
-              <th className="lapTime">lap</th>
-            </tr>
-          </thead>
-        </table>
+  render() {
+    const { driver, sessionBests } = this.props;
+    const dhLaps = [];
+    if (driver === undefined) {
+      return (<div>No driver selected</div>);
+    }
+    const driverBests = driver.get('best');
+    driver.get('laps').toArray().forEach((lap) => {
+      dhLaps.push(<DriverHistoryLap key={lap.get('lapNumber')} lap={lap} driverBests={driverBests} sessionBests={sessionBests} />);
+    });
+
+    return (
+      <div className="flex-container">
+        <div className="timing-table-header">
+          <table className="table table-striped timing-table driver-history">
+            <thead>
+              <tr>
+                <th className="lapNumber">#</th>
+                <th className="s1Time">s1</th>
+                <th className="s2Time">s2</th>
+                <th className="s3Time">s3</th>
+                <th className="lapTime">lap</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div className="scrolling" ref={(d) => { this.scrollingContainer = d; }}>
+          <table className="table table-striped timing-table driver-history">
+            <tbody>
+              {dhLaps}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div className="scrolling">
-        <table className="table table-striped timing-table driver-history">
-          <tbody>
-            {dhLaps}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 DriverHistory.propTypes = {
   driver: PropTypes.instanceOf(Immutable.Map),
