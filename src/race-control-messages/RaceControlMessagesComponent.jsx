ef0411c3ff6c4757dce52122001d6306a29195c3
David Green <dave@activelylazy.co.uk>
2017-06-12 18:22:32 +0100

    Add key to each message row

--- src/race-control-messages/RaceControlMessagesComponent.jsx
+++ src/race-control-messages/RaceControlMessagesComponent.jsx
@@ -4,7 +4,7 @@ import { PropTypes } from 'prop-types';
 import { toTimeOfDay } from '../session-data/timing-utils';
 
 const RaceControlMessageRow = msg => (
-  <div className="row">
+  <div className="row" key={msg.get('timestamp')}>
     <div className="col-md-1 timestamp">{toTimeOfDay(msg.get('timestamp'))}</div>
     <div className="col-md-11 message">{msg.get('message')}</div>
   </div>
