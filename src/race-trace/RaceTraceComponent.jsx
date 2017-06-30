85ee05cff659bd7c2bbdc067498e9111ff52790c
David Green <dave@activelylazy.co.uk>
2017-06-30 17:39:30 +0100

    Implement auto-scaling

--- src/race-trace/RaceTraceComponent.jsx
+++ src/race-trace/RaceTraceComponent.jsx
@@ -1,9 +1,81 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import Immutable from 'immutable';
 import $ from 'jquery';
 import flot from 'flot-for-node'; // eslint-disable-line no-unused-vars
 import normaliseTimes from './normalise-times';
 import plotStructure from './plot-structure';
+
+function createChartOptions(minMax) {
+  return {
+    xaxis: {
+      show: true,
+      color: 'white',
+      tickColor: 'white',
+      font: {
+        size: 10,
+        lineHeight: 13,
+        family: 'Lato,Helvetica Neue,Helvetica,Arial,sans-serif',
+        variant: 'small-caps',
+        color: '#CCCCCC',
+      },
+    },
+    yaxis: {
+      show: true,
+      color: 'white',
+      tickColor: 'white',
+      min: minMax.min,
+      max: minMax.max,
+      font: {
+        size: 10,
+        lineHeight: 13,
+        family: 'Lato,Helvetica Neue,Helvetica,Arial,sans-serif',
+        variant: 'small-caps',
+        color: '#CCCCCC',
+      },
+    },
+    lines: {
+      lineWidth: 0.5,
+    },
+    grid: {
+      borderWidth: 0.5,
+      borderColor: 'white',
+      xhoverable: true,
+      xclickable: true,
+    },
+    legend: {
+      backgroundColor: 'transparent',
+    },
+  };
+}
+
+function findMinMax(times, selectedDriver, selectedOpponent) {
+  if (selectedDriver !== '') {
+    const driverTimes = times.get(selectedDriver).filter(t => !isNaN(t));
+    let min = driverTimes.min();
+    let max = driverTimes.max();
+    if (selectedOpponent !== '') {
+      min = Math.min(min, times.get(selectedOpponent).filter(t => !isNaN(t)).min());
+      max = Math.max(max, times.get(selectedOpponent).filter(t => !isNaN(t)).max());
+    }
+    return {
+      min,
+      max,
+    };
+  }
+  if (times.keySeq().length === 0) {
+    return {
+      min: -5,
+      max: 50,
+    };
+  }
+  const allTimes = times.valueSeq().flatMap(v => v).filter(t => !isNaN(t));
+  return {
+    min: allTimes.min(),
+    max: allTimes.max(),
+  };
+}
 
 class RaceTraceComponent extends React.Component {
   componentDidMount() {
@@ -25,84 +97,34 @@ class RaceTraceComponent extends React.Component {
     $(this.refs.plotContainer).width(w * 0.48);
     $(this.refs.plotContainer).height(h * 0.44);
 
-    const chartOptions = {
-      xaxis: {
-        show: true,
-        color: 'white',
-        tickColor: 'white',
-        font: {
-          size: 11,
-          lineHeight: 13,
-          family: 'sans-serif',
-          variant: 'small-caps',
-          color: '#CCCCCC',
-        },
-      },
-      yaxis: {
-        show: true,
-        color: 'white',
-        tickColor: 'white',
-        min: -10,
-        max: 50,
-        font: {
-          size: 11,
-          lineHeight: 13,
-          family: 'sans-serif',
-          variant: 'small-caps',
-          color: '#CCCCCC',
-        },
-      },
-      lines: {
-        lineWidth: 0.5,
-      },
-      grid: {
-        borderWidth: 0,
-        xhoverable: true,
-        xclickable: true,
-      },
-      legend: {
-        backgroundColor: 'transparent',
-      },
-    };
-    // const chartData = [
-    //   {
-    //     label: 'VET',
-    //     shadowSize: 0,
-    //     xlines: { lineWidth: 5 },
-    //     color: '#FF0000',
-    //     data: [[0, 3], [10, 30], [20, 45], [30, 40]],
-    //   },
-    //   {
-    //     label: 'HAM',
-    //     shadowSize: 0,
-    //     color: '#CCCCCC',
-    //     data: [[0, 2], [10, 28], [20, 47], [30, 41]],
-    //   }];
     const session = this.props.session;
     const normalTimes = normaliseTimes(session);
     const chartData = plotStructure(normalTimes, this.props.selectedDriver, this.props.selectedOpponent);
+    const chartOptions = createChartOptions(findMinMax(normalTimes, this.props.selectedDriver, this.props.selectedOpponent));
 
     if (chartData.length === 0) {
       return;
     }
 
-    // $(this.refs.plotContainer).unbind('plothover', this.plotHover.bind(this));
     this.plot = $.plot(this.refs.plotContainer, chartData, chartOptions);
-    // $(this.refs.plotContainer).bind('plothover', this.plotHover.bind(this));
   }
-  // plotHover(event, pos, item) {
-  //   if (item !== null) {
-  //     console.log(`hover on ${JSON.stringify(item)}`);
-  //     item.series.lines.width = 5;
-  //     this.plot.draw();
-  //   }
-  // }
   render() {
     return (
       <div className="race-trace" ref="plotContainer" />
     );
   }
 }
+
+RaceTraceComponent.propTypes = {
+  session: PropTypes.instanceOf(Immutable.Map).isRequired,
+  selectedDriver: PropTypes.string,
+  selectedOpponent: PropTypes.string,
+};
+
+RaceTraceComponent.defaultProps = {
+  selectedDriver: '',
+  selectedOpponent: '',
+};
 
 function mapStateToProps(state) {
   return {
