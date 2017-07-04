78fd300d83735f4bece29e4e590981b3a21540a3
David Green <dave@activelylazy.co.uk>
2017-07-04 18:08:26 +0100

    Show seconds until connection

--- src/main-view/MainViewSwitcher.jsx
+++ src/main-view/MainViewSwitcher.jsx
@@ -4,22 +4,28 @@ import { connect } from 'react-redux';
 import MainView from './MainViewComponent';
 import NotActive from './NotActiveComponent';
 
-const MainViewContainer = ({ active }) => {
+const MainViewContainer = ({ active, secondsUntilConnect }) => {
   if (active) {
     return (
       <MainView />
     );
   }
-  return (<NotActive />);
+  return (<NotActive secondsUntilConnect={secondsUntilConnect} />);
 };
 
 MainViewContainer.propTypes = {
   active: PropTypes.bool.isRequired,
+  secondsUntilConnect: PropTypes.number,
+};
+
+MainViewContainer.defaultProps = {
+  secondsUntilConnect: NaN,
 };
 
 function mapStateToProps(state) {
   return {
     active: state.session.get('active'),
+    secondsUntilConnect: state.session.get('secondsUntilConnect'),
   };
 }
 
--- src/main-view/NotActiveComponent.jsx
+++ src/main-view/NotActiveComponent.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
-const NotActiveComponent = () => (
-  <div>no session active</div>
+function toSimpleTime(time) {
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor((time - (hours * 3600)) / 60);
+  let seconds = time - (hours * 3600) - (minutes * 60);
+
+  if (hours < 10) { hours = `0${hours}`; }
+  if (minutes < 10) { minutes = `0${minutes}`; }
+  if (seconds < 10) { seconds = `0${seconds}`; }
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function getConnectMessage(time) {
+  if (isNaN(time)) {
+    return undefined;
+  }
+  return (<div>Connecting in <b>{toSimpleTime(time)}</b></div>);
+}
+
+const NotActiveComponent = ({ secondsUntilConnect }) => (
+  <div className="row">
+    <div className="col-md-4" />
+    <div className="col-md-4 panel not-active-timer">
+      There is no session currently active <br /><br />
+      {getConnectMessage(secondsUntilConnect)}
+    </div>
+  </div>
 );
+
+NotActiveComponent.propTypes = {
+  secondsUntilConnect: PropTypes.number.isRequired,
+};
 
 export default NotActiveComponent;
 
