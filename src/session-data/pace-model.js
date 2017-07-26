5f4782cd17c124a1898b0726f0c8e5cd48139f99
David Green <dave@activelylazy.co.uk>
2017-07-26 20:34:02 +0100

    Add pace model

--- src/session-data/pace-model.js
+++ src/session-data/pace-model.js
@@ -1,122 +1,150 @@
 import { fromJS } from 'immutable';
 import math from 'mathjs';
 
-function median(values) {
-  if (values.length === 0) {
-    return undefined;
-  }
-  if (values.length % 2 === 1) {
-    return values[(values.length - 1) / 2];
-  }
-  return (values[values.length / 2] + values[(values.length / 2) - 1]) / 2;
+function toHeaderArray(referenceTyre, tyres, drivers) {
+  const r = [];
+  r.push('raceLapIndex');
+  tyres.forEach(tyre => r.push(`${tyre} stint lap`));
+  tyres.forEach((tyre) => {
+    if (tyre !== referenceTyre) {
+      r.push(`${tyre} delta vs ${referenceTyre}`);
+    }
+  });
+  drivers.forEach(d => r.push(`${d} pace`));
+  r.push('lap time');
+  return r;
 }
 
-function forTyre(lap, tyre, value) {
-  if (lap.tyre === tyre) {
-    return value;
-  }
-  return 0;
-}
-
-// create matrix with
+// row of:
 //   raceLapIndex
-//   1 if tyre == 'S', 0 o/w
-//   stintLapIndex if tyre == 'S', 0 o/w
-//   1 if tyre == 'M', 0 o/w
-//   stintLapIndex if tyre == 'M', 0 o/w
-//   ...etc... for each usableTyre
-function prepareMatrixRow(lap, usableTyres) {
-  const row = [lap.raceLapIndex];
-  usableTyres.forEach((tyre) => {
-    row.push(forTyre(lap, tyre, 1));
-    row.push(forTyre(lap, tyre, lap.stintLapIndex));
+//   if S, stintLapIndex, 0 o/w
+//   if M, stintLapIndex, 0 o/w
+//   ...
+//   for each non-reference tyre, 1 or 0
+//   if driver1: 1, 0 o/w
+//   if driver2: 1, 0 o/w
+//   ...
+function toCsvArrayLap(lap, driver, referenceTyre, tyres, drivers) {
+  const r = [];
+  r.push(lap.raceLapIndex);
+  tyres.forEach((tyre) => {
+    if (lap.tyre === tyre) {
+      r.push(lap.stintLapIndex);
+    } else {
+      r.push(0);
+    }
   });
-  return row;
-}
-
-// translates the theta from the above matrix row to a result structure
-function fromVectorToResult(theta, usableTyres) {
-  const result = {
-    fuelEffect: theta.subset(math.index(0, 0)),
-    tyreModel: {},
-  };
-  let index = 1;
-  usableTyres.forEach((tyre) => {
-    result.tyreModel[tyre] = {
-      offset: theta.subset(math.index(index++, 0)),  // eslint-disable-line
-      gain: theta.subset(math.index(index++, 0)),   // eslint-disable-line
-    };
+  tyres.forEach((tyre) => {
+    if (tyre !== referenceTyre) {
+      if (lap.tyre === tyre) {
+        r.push(1);
+      } else {
+        r.push(0);
+      }
+    }
   });
-  return result;
+  drivers.forEach((d) => {
+    if (driver === d) {
+      r.push(1);
+    } else {
+      r.push(0);
+    }
+  });
+  r.push(lap.lapTime);
+  return r;
 }
 
-function findParams(freeAirLaps) {
-  const lapsByTyre = fromJS(freeAirLaps.map(lap => lap.tyre)).groupBy(tyre => tyre);
-  const usableTyres = lapsByTyre.keySeq().filter(tyre => lapsByTyre.get(tyre).count() >= 5);
-  const usableLaps = freeAirLaps.filter(lap => usableTyres.contains(lap.tyre));
-
-  if (usableLaps.length === 0) {
-    return {};
+function toCsvArray(laps, driver, referenceTyre, tyres, drivers) {
+  if (laps === undefined) {
+    return [];
   }
-
-  const X = math.matrix(usableLaps.map(lap => prepareMatrixRow(lap, usableTyres)));
-  const y = math.transpose(math.matrix([usableLaps.map(lap => lap.lapTime)]));
-
-  // theta = inv(X' * X) * X' * y
-  let theta;
-  try {
-    theta = math.multiply(math.multiply(math.inv(math.multiply(math.transpose(X), X)),
-                                        math.transpose(X)), y);
-  } catch (e) {
-    return {};
-  }
-
-  const hyp = math.multiply(X, theta);
-
-  const meanSquaredError = math.sum(math.square(math.subtract(hyp, y))) /
-                              math.size(y).subset(math.index(0));
-
-  return Object.assign(fromVectorToResult(theta, usableTyres), { meanSquaredError });
+  return laps.map(lap => toCsvArrayLap(lap, driver, referenceTyre, tyres, drivers));
 }
 
-function findMedianTyreDeg(usableParams, tyre) {
-  return median(usableParams
-              .filter(p => p !== undefined &&
-                           p.tyreModel[tyre] !== undefined &&
-                           !isNaN(p.tyreModel[tyre].gain))
-              .map(p => p.tyreModel[tyre].gain)
-              .sort().toArray());
+function getModelLaps(freeAirLaps, tyres, driverList) {
+  const rows = [];
+  driverList.forEach((tla) => {
+    toCsvArray(freeAirLaps.get(tla), tla, tyres[0], tyres, driverList).forEach(x => rows.push(x));
+  });
+  return rows;
 }
+function dumpCsv(freeAirLaps, tyres, driverList) {
+  const headerRow = toHeaderArray(tyres[0], tyres, driverList);
+  const rows = [headerRow, ...getModelLaps(freeAirLaps, tyres, driverList)];
 
-const concat = (x, y) => x.concat(y);
-const flatmap = (xs, f) => xs.map(f).reduce(concat, []);
+  console.log(`read:\n${rows.join('\n')}`);
+}
 
 function calcModel(drivers, freeAirLaps) {
-  const allParams = freeAirLaps.map((lap, driver) => findParams(lap, driver));
+  if (freeAirLaps.size === 0) {
+    return {};
+  }
 
-  const usableParams = allParams.filter(p => p.meanSquaredError < 0.5);
+  const foundTyres = [...new Set(freeAirLaps.toList().flatMap(laps => fromJS(laps).map(lap => lap.get('tyre'))))];
+  const interestingTyres = ['E', 'V', 'S', 'M', 'H'];
+  const tyres = interestingTyres.filter(t => foundTyres.includes(t));
 
-  const fuelGains = usableParams
-                .filter(p => p !== undefined && !isNaN(p.fuelEffect))
-                .map(p => p.fuelEffect)
-                .sort();
+  // get drivers for whom we have free air laps
+  const driverList = freeAirLaps
+    .filter(laps => laps.length > 1)
+    .map((laps, driver) => driver).toArray();
+
+  // dumpCsv(freeAirLaps, tyres, driverList);
+
+  const modelLaps = math.matrix(getModelLaps(freeAirLaps, tyres, driverList));
+
+  const numDrivers = driverList.length;
+  const numLaps = math.subset(math.size(modelLaps), math.index(0));
+  const numCols = math.subset(math.size(modelLaps), math.index(1));
+  const numTyres = (numCols - numDrivers - 1) / 2;
+
+  const X = math.subset(modelLaps, math.index(math.range(0, numLaps), math.range(0, numCols - 1)));
+  const y = math.subset(modelLaps, math.index(math.range(0, numLaps), math.range(numCols - 1, numCols)));
+  const lambda = 0.01;
+
+  let reg = math.zeros(numCols - 1, numCols - 1);
+  // we want to regularize the tyre delta column(s)
+  for (let i = 3; i < 2 + numTyres; i++) { // eslint-disable-line
+    reg = math.subset(reg, math.index(i, i), 1);
+  }
+
+  // theta = inv((X' * X) + (lambda * reg)) * X' * y
+  let theta;
+  try {
+    theta = math.multiply(math.multiply(
+                math.inv(
+                  math.add(
+                    math.multiply(math.transpose(X), X),
+                    math.multiply(lambda, reg))),
+                math.transpose(X)),
+            y);
+  } catch (e) {
+    console.log(`error: ${e}`);
+    return {};
+  }
 
   const model = {
-    fuelEffect: median(fuelGains.toArray()),
-    tyreModel: { },
-    drivers: {},
+    fuelEffect: math.subset(theta, math.index(0, 0)),
+    tyreModel: {
+      deg: {},
+      baseTyre: tyres[0],
+      delta: {},
+    },
+    driverModel: {},
   };
 
-  const allTyres = [...new Set(flatmap(usableParams, p => Object.keys(p.tyreModel)))];
-  allTyres.forEach((tyre) => {
-    model.tyreModel[tyre] = findMedianTyreDeg(usableParams, tyre);
+  tyres.forEach((tyre, index) => {
+    model.tyreModel.deg[tyre] = math.subset(theta, math.index(1 + index, 0));
+    if (index > 0) {
+      model.tyreModel.delta[tyre] = math.subset(theta, math.index(tyres.length + index, 0));
+    }
   });
 
-  drivers.forEach((driver) => {
-    model.drivers[driver.get('tla')] = usableParams.get(driver.get('tla'));
+  driverList.forEach((driver, index) => {
+    model.driverModel[driver] = math.subset(theta, math.index((tyres.length * 2) + index, 0));
   });
 
-  console.log(`model is ${JSON.stringify(model)}`);
+  console.log(`got model: ${JSON.stringify(model)}`);
 
   return model;
 }
