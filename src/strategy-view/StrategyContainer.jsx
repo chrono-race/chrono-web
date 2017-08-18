849d713064528c2c7fed3604cd9f260aae7fb814
David Green <dave@activelylazy.co.uk>
2017-08-18 20:59:19 +0100

    Add sliders to strategy view

--- src/strategy-view/StrategyContainer.jsx
+++ src/strategy-view/StrategyContainer.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import Immutable from 'immutable';
+import ReactBootstrapSlider from 'react-bootstrap-slider';
 import { updateStrategies } from './strategy-actions';
+import sliderCss from '../css/bootstrap-slider.min.css'; // eslint-disable-line no-unused-vars
 
 function makeCell(driverLap, opponentLap, setStrategy) {
   return (
     <td key={`${driverLap}-${opponentLap}`}>
-      <a tabIndex="-1" onClick={() => setStrategy(driverLap, opponentLap)}>{driverLap}/{opponentLap}</a>
+      <a
+        tabIndex="-1"
+        onClick={() => setStrategy(driverLap, opponentLap)}
+      >xx</a>
     </td>);
 }
 
@@ -22,7 +27,7 @@ function makeRow(driverLap, setStrategy) {
 
 function makeHeaderRow() {
   const cells = [...Array(21).keys()]
-    .map(lap => (<th>{lap + 1}</th>));
+    .map(lap => (<th key={lap}>{lap + 1}</th>));
   return (<tr key="header">
     <td />
     {cells}
@@ -48,6 +53,28 @@ const StrategyContainer = ({ session, setStrategy }) => {
     <div className="strategy-container">
       <table>
         <tbody>
+          <tr>
+            <td />
+            <td colSpan="21">
+              <div className="opponent-lap-slider">
+                <ReactBootstrapSlider
+                  value={21}
+                  min={1}
+                  max={21}
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td rowSpan="23">
+              <ReactBootstrapSlider
+                value={21}
+                min={1}
+                max={21}
+                orientation="vertical"
+              />
+            </td>
+          </tr>
           {rows}
         </tbody>
       </table>
